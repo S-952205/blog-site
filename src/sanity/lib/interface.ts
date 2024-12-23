3fbf54cf6493784cd87efbd13162909466162bc8
Syed Sufyan <sufyansyed157@gmail.com>
2024-12-23 18:57:47 +0500

    added type for article in interface

--- src/sanity/lib/interface.ts
+++ src/sanity/lib/interface.ts
@@ -9,5 +9,13 @@ export interface blogallpost{
     title: 'string',
     currentSlug: 'string',  
     imageUrl: string,
-    article: { type: string; content: string }[];
-}
+    article: {
+        _type: string;          // e.g., 'block'
+        children: {
+          _type: string;        // e.g., 'span' for text
+          text: string;         // The actual text content
+          marks: string[];      // Marks like bold, italic, etc.
+        }[];
+        style?: string;         // Optional, e.g., 'normal', 'h1', etc.
+      }[];
+    }
